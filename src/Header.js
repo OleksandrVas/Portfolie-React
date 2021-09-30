35642dd1d0b4c3c5644ed4b80974a0d4e84bd132
Oleksandr <sasha228123@gmail.com>
2021-09-30 16:00:31 +0300

    added compontnts

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <li>
+        <h1>My world before touch </h1>
+      </li>
+    </>
+  );
+};
+
+export default Header
---
+++ src/TechJs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tex = () => {
+  return (
+    <>
+      <ul>
+        <li>REact</li>
+        <li>JS</li>
+        <li>Jquary</li>
+      </ul>
+    </>
+  );
+};
+
+export default Tex;
