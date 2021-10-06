260750d6c7076b8356c3b4f606849f1368c821ad
Oleksandr <sasha228123@gmail.com>
2021-10-06 22:04:08 +0300

    added navLink to Nav

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import Profile from "../src/Components/Profile/Profile";
 import Header from "../src/Components/Header/Header"
 import Nav from "../src/Components/NavBar/Nav"
+import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+
 const App = () => {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/news" component={Profile}/>
+                    <Route path="/music" component={Dialogs}/>
+                    <Route path="/settings" component={Profile}/>
+                </div>
+
+            </div>
+        </BrowserRouter>
+
+    );
 };
 export default App;
