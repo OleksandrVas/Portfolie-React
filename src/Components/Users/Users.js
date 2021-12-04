684958b20f745870bb7853ada4f6a3ee16d81f31
Oleksand <sasha228123@gmail.com>
2021-12-04 18:29:35 +0200

    added peginations to UsersPage

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -3,18 +3,47 @@ import classes from "./users.module.css"
 import axios from "axios";
 import userDefaultPhoto from "../../assets/img/defaultUserImg.png"
 
- class Users extends React.Component{
-
+class Users extends React.Component {
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
             })
     }
 
-     render() {
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+
+            })
+
+    }
+
+    render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+        let pages = [];
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return (
             <div>
+                <div>
+                    {pages.map(p => {
+                        return (
+                            <span className={this.props.currentPage === p && classes.selecetedPage}
+                                  onClick={(e) => {
+                                      this.onPageChanged(p)
+                                  }}>{p}</span>
+                        )
+                    })}
+                </div>
+
                 {
                     this.props.users.map(u => <div key={u.id}>
                         <span>
@@ -52,6 +81,6 @@ import userDefaultPhoto from "../../assets/img/defaultUserImg.png"
         )
 
     }
- }
+}
 
- export default Users
+export default Users
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = "SET-USERS"
+const SET_CURRENT_PAGE = "SET-CURRENT-PAGE"
+const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT"
 
 let initialState = {
     users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 }
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -33,9 +38,18 @@ const userReducer = (state = initialState, action) => {
             }
 
         case SET_USERS :
-            return  {
-                ...state , users : [...state.users , ...action.users ]
+            return {
+                ...state, users: action.users
             }
+        case SET_CURRENT_PAGE :
+            return {
+                ...state, currentPage: action.currentPage
+            }
+        case SET_TOTAL_USERS_COUNT :
+            return {
+                ...state, totalUsersCount : action.count
+            }
+
         default :
             return state
 
@@ -43,8 +57,10 @@ const userReducer = (state = initialState, action) => {
     }
 }
 export const followAC = (userId) => ({type: FOLLOW, userId});
-export const unFollowAC = (userId) => ({type: UNFOLLOW,  userId});
-export const setUsersAC = (users) => ({type: SET_USERS , users });
+export const unFollowAC = (userId) => ({type: UNFOLLOW, userId});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage: currentPage})
+export const setTotalUsersCountAC = (totalUserCount) => ({type: SET_TOTAL_USERS_COUNT, count : totalUserCount })
 
 
 export default userReducer
