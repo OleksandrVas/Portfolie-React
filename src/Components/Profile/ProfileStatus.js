8a07dd250cff568f79b9744045d8ce3b9410eaf3
Oleksand <sasha228123@gmail.com>
2021-12-27 15:52:18 +0200

    added componentDidUpdate for ProfileStatus

--- src/Components/Profile/ProfileStatus.js
+++ src/Components/Profile/ProfileStatus.js
@@ -31,9 +31,19 @@ class ProfileStatus extends React.Component {
         console.log(e.currentTarget.value)
     }
 
+   componentDidUpdate(prevProps, prevStatus,snapshot){
+        if(prevProps.status !== this.props.status){
+            this.setState({
+                status : this.props.status
+            })
+        }
+   }
+
 
     render() {
+
         return (
+
             <>
                 {!this.state.editMode &&
                 <div>
